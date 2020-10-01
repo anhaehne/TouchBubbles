42931bcda902fd54f346654942c2d6cf70c54a75
Andre Hähnel <haehnel@adremes.com>
2020-10-01 16:14:45 +0200

    Initial commit

---
+++ src/TouchBubbles/Client/wwwroot/js/swiper/swiper-interop.js
@@ -0,0 +1,11 @@
+﻿window.Swiper.Initialize = () => {
+    window.Swiper.obj = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        centeredSlides: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        slideToClickedSlide: true,
+    })
+}
